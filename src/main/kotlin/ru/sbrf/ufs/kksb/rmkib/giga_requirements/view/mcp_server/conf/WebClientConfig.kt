517879e4520bc6f378e928b08656fe6179944c11
denis <hunter-21@yandex.ru>
2025-03-20 22:18:03 +0300

    changes

--- src/main/kotlin/ru/sbrf/ufs/kksb/rmkib/giga_requirements/view/mcp_server/conf/WebClientConfig.kt
+++ src/main/kotlin/ru/sbrf/ufs/kksb/rmkib/giga_requirements/view/mcp_server/conf/WebClientConfig.kt
@@ -3,22 +3,21 @@ package ru.sbrf.ufs.kksb.rmkib.giga_requirements.view.mcp_server.conf
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.io.Resource
+import org.springframework.context.annotation.PropertySource
 import org.springframework.web.reactive.function.client.WebClient
-import java.util.*
 
 @Configuration
+@PropertySource("classpath:application-secret.properties")
 open class WebClientConfig {
 
-    /*@Value("classpath:token.txt")
-    private lateinit var tokenResource: Resource*/
+    @Value("auth.token")
+    private lateinit var authToken: String
 
     @Bean
     open fun webClient(): WebClient {
-        //val token = Scanner(tokenResource.inputStream).useDelimiter("\\A").next().trim()
         return WebClient.builder()
             .baseUrl("https://elizavetamartinovich1410.atlassian.net/wiki")
-            .defaultHeader("Authorization", "Basic ${"token"}")
+            .defaultHeader("Authorization", "Basic $authToken")
             .defaultHeader("Content-Type", "application/json")
             .build()
     }
