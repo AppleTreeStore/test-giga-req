e9d76406de3279d9a657807c3e0d46359b1aa634
denis <hunter-21@yandex.ru>
2025-03-20 17:17:31 +0300

    changes

--- src/main/kotlin/ru/sbrf/ufs/kksb/rmkib/giga_requirements/view/mcp_server/models/confluence_pages/rq/ConfluencePageRq.kt
+++ src/main/kotlin/ru/sbrf/ufs/kksb/rmkib/giga_requirements/view/mcp_server/models/confluence_pages/rq/ConfluencePageRq.kt
@@ -1,5 +1,8 @@
 package ru.sbrf.ufs.kksb.rmkib.giga_requirements.view.mcp_server.models.confluence_pages.rq
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class ConfluencePageRq(
+    @JsonProperty("id")
     var id: String? = null
 )
