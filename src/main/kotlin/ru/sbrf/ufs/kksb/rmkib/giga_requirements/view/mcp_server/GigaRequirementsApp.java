676ae3a2b46e23beb3f75a9d391e6b281773d7e5
denis <hunter-21@yandex.ru>
2025-03-20 14:47:46 +0300

    changes

--- src/main/kotlin/ru/sbrf/ufs/kksb/rmkib/giga_requirements/view/mcp_server/GigaRequirementsApp.java
+++ src/main/kotlin/ru/sbrf/ufs/kksb/rmkib/giga_requirements/view/mcp_server/GigaRequirementsApp.java
@@ -2,8 +2,10 @@ package ru.sbrf.ufs.kksb.rmkib.giga_requirements.view.mcp_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"ru.sbrf.ufs.kksb.rmkib.giga_requirements.view.mcp_server.tools"})
 public class GigaRequirementsApp {
     public static void main(String[] args) {
         SpringApplication.run(GigaRequirementsApp.class, args);
