24348a8f53ef08a47b64080766d6e9cb54ad0fef
denis <hunter-21@yandex.ru>
2025-03-20 23:55:41 +0300

    changes

--- src/main/kotlin/ru/sbrf/ufs/kksb/rmkib/giga_requirements/view/mcp_server/conf/WebClientConfig.kt
+++ src/main/kotlin/ru/sbrf/ufs/kksb/rmkib/giga_requirements/view/mcp_server/conf/WebClientConfig.kt
@@ -9,14 +9,11 @@ import org.springframework.web.reactive.function.client.WebClient
 @Configuration
 open class WebClientConfig {
 
-    @Value("auth.token")
-    private lateinit var authToken: String
-
     @Bean
     open fun webClient(): WebClient {
         return WebClient.builder()
             .baseUrl("https://elizavetamartinovich1410.atlassian.net/wiki")
-            .defaultHeader("Authorization", "Basic $authToken")
+            .defaultHeader("Authorization", "Basic ${System.getenv("auth-token")}")
             .defaultHeader("Content-Type", "application/json")
             .build()
     }
