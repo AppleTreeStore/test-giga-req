187ce7bbfb968ae85d2fac456f633e8111be123c
denis <hunter-21@yandex.ru>
2025-03-20 17:20:35 +0300

    changes

--- src/main/kotlin/ru/sbrf/ufs/kksb/rmkib/giga_requirements/view/mcp_server/tools/ConfluencePagesTools.kt
+++ src/main/kotlin/ru/sbrf/ufs/kksb/rmkib/giga_requirements/view/mcp_server/tools/ConfluencePagesTools.kt
@@ -1,6 +1,7 @@
 package ru.sbrf.ufs.kksb.rmkib.giga_requirements.view.mcp_server.tools
 
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.sbrf.ufs.kksb.rmkib.giga_requirements.view.mcp_server.controller.api.ConfluencePagesControllerApi
@@ -13,7 +14,7 @@ class ConfluencePagesTools(
     private val confluencePagesControllerApi: ConfluencePagesControllerApi
 ) {
     @PostMapping("/current-page")
-    fun getCurrentConfluencePage(rq: ConfluencePageRq): ConfluencePageRs {
+    fun getCurrentConfluencePage(@RequestBody rq: ConfluencePageRq): ConfluencePageRs {
         return confluencePagesControllerApi.getCurrentConfluencePage(rq)
     }
 
