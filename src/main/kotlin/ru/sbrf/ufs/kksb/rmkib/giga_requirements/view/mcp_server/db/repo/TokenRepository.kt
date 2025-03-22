19ce52f28f39a6839ac2226a49f4040d0adfb177
denis <hunter-21@yandex.ru>
2025-03-22 13:53:52 +0300

    changes db

--- src/main/kotlin/ru/sbrf/ufs/kksb/rmkib/giga_requirements/view/mcp_server/db/repo/TokenRepository.kt
+++ src/main/kotlin/ru/sbrf/ufs/kksb/rmkib/giga_requirements/view/mcp_server/db/repo/TokenRepository.kt
@@ -1,6 +1,8 @@
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import ru.sbrf.ufs.kksb.rmkib.giga_requirements.view.mcp_server.db.entity.Token
 
+@Repository
 interface TokenRepository : JpaRepository<Token, Long> {
     fun findByToken(token: String): Token?
 }
