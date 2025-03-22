b5edb1754c8afc0d182d2a1396841d1edd7eb87c
denis <hunter-21@yandex.ru>
2025-03-22 14:42:01 +0300

    changes db

--- src/main/kotlin/ru/sbrf/ufs/kksb/rmkib/giga_requirements/view/mcp_server/models/common/BaseResponse.kt
+++ src/main/kotlin/ru/sbrf/ufs/kksb/rmkib/giga_requirements/view/mcp_server/models/common/BaseResponse.kt
@@ -1,14 +1,19 @@
 package ru.sbrf.ufs.kksb.rmkib.giga_requirements.view.mcp_server.models.common
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.slf4j.Logger
 import ru.sbrf.ufs.kksb.rmkib.giga_requirements.view.mcp_server.errors.CommonError
 import ru.sbrf.ufs.kksb.rmkib.giga_requirements.view.mcp_server.errors.GigaRequirementsIntegrationError
 import java.util.UUID
 
 data class BaseResponse<T>(
+    @JsonProperty("body")
     private var body: T? = null,
+    @JsonProperty("success")
     private var success: Boolean? = null,
+    @JsonProperty("error")
     private var error: CommonError? = null) {
+
      companion object {
          fun <T> success(logger: Logger, body: () -> T): BaseResponse<T> {
              val result = try {
