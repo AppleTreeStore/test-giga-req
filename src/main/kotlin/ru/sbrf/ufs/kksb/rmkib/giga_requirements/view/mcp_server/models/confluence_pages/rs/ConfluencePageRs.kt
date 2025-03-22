3dc7dfc26d259e2ddfc92d610241fd367c069609
denis <hunter-21@yandex.ru>
2025-03-22 14:31:56 +0300

    changes db

--- src/main/kotlin/ru/sbrf/ufs/kksb/rmkib/giga_requirements/view/mcp_server/models/confluence_pages/rs/ConfluencePageRs.kt
+++ src/main/kotlin/ru/sbrf/ufs/kksb/rmkib/giga_requirements/view/mcp_server/models/confluence_pages/rs/ConfluencePageRs.kt
@@ -1,5 +1,8 @@
 package ru.sbrf.ufs.kksb.rmkib.giga_requirements.view.mcp_server.models.confluence_pages.rs
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class ConfluencePageRs(
+    @JsonProperty("data")
     var data: String? = null
 )
